874ec45a16066cc639b4bb9f6f0d8a1b3cc3c299
Claylton Santos <clayltonsp@hotmail.com>
2024-08-13 11:07:47 -0300

    🌈 style: insert assets
    Add assets, like animations, fonts and colors

---
+++ lib/themes/themes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const brightness = Brightness.dark;
+const primaryColor = Color(0xFFFDBF00);
+const accentColor = Color(0xFFFFFFFF);
+
+ThemeData appTheme() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      brightness: brightness,
+      seedColor: primaryColor,
+      secondary: accentColor,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(fontFamily: "Montserrat"),
+      bodyMedium: TextStyle(fontFamily: "Montserrat"),
+      bodySmall: TextStyle(fontFamily: "Montserrat"),
+      displayLarge: TextStyle(fontFamily: "Montserrat"),
+      displayMedium: TextStyle(fontFamily: "Montserrat"),
+      displaySmall: TextStyle(fontFamily: "Montserrat"),
+      headlineLarge: TextStyle(fontFamily: "Montserrat"),
+      headlineMedium: TextStyle(fontFamily: "Montserrat"),
+      headlineSmall: TextStyle(fontFamily: "Montserrat"),
+      labelLarge: TextStyle(fontFamily: "Montserrat"),
+      labelMedium: TextStyle(fontFamily: "Montserrat"),
+      labelSmall: TextStyle(fontFamily: "Montserrat"),
+      titleLarge: TextStyle(fontFamily: "Montserrat"),
+      titleMedium: TextStyle(fontFamily: "Montserrat"),
+      titleSmall: TextStyle(fontFamily: "Montserrat"),
+    ),
+  );
+}
