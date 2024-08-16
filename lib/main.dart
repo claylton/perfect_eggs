caf74cc529a4cbce1f817a7d070e189abec790e4
Claylton Santos <clayltonsp@hotmail.com>
2024-08-16 03:58:18 -0300

    ✨ feat: Create bloc and Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:perfect_eggs/blocs/app_bloc.dart';
 import 'package:perfect_eggs/pages/splash_page.dart';
 import 'package:perfect_eggs/themes/themes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +10,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppBloc>.value(
+          value: AppBloc(),
+        )
+      ],
+      child: const Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
