b2030b566644e8e56036bd72861ef187942bbe0b
Claylton Santos <clayltonsp@hotmail.com>
2024-08-16 12:57:03 -0300

    🦄 refactor: refactor home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:perfect_eggs/blocs/app_bloc.dart';
 import 'package:perfect_eggs/widgets/action_button_widget.dart';
 import 'package:perfect_eggs/widgets/eggs_widget.dart';
 import 'package:perfect_eggs/widgets/progress_widget.dart';
 import 'package:perfect_eggs/widgets/type_buttons_widget.dart';
-import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final AppBloc bloc = Provider.of<AppBloc>(context);
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 60),
-          Text(bloc.selected),
-          Text(bloc.state),
-          Text(bloc.time.toString()),
-          Text(bloc.seconds.toString()),
-          const TypeButtonsWidget(),
-          const SizedBox(height: 10),
-          const ActionButtonWidget(),
-          const SizedBox(height: 10),
-          const ProgressWidget(),
-          const SizedBox(height: 10),
-          const EggsWidget(),
-        ],
-      ),
-    );
+        body: ListView(
+          children: const [
+            SizedBox(height: 50),
+            Text(
+              "Perfect Eggs",
+              style: TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            EggsWidget(),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TypeButtonsWidget(),
+            ),
+            ProgressWidget(),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: ActionButtonWidget(),
+            )
+          ],
+        ));
   }
 }
